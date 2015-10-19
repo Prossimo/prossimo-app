436c9a84cb49f74722b892a03771524f97b8f092
lavrton <lavrton@gmail.com>
2015-10-19 13:56:55 +0700

    mullion drawing

--- static/source/js/drawing-windows/views/main-drawing-windows-view.js
+++ static/source/js/drawing-windows/views/main-drawing-windows-view.js
@@ -12,7 +12,23 @@ var app = app || {};
             //  containing collection of `Window` models, and each instance of
             //  `Window` would probably have a `WindowDrawing` stored inside
             this.active_drawing = new app.WindowDrawing();
+            this.active_drawing.set({
+                width: 1000,
+                height: 2000
+            });
+            this.listenTo(this.active_drawing, 'all', this.updateCanvas);
         },
+        events: {
+            'click .add-vertical-million': 'addVerticalMullion',
+            'click .add-horizontal-million': 'addHorizontalMullion',
+            'click .popup-wrap': function(e) {
+                var el = $(e.target);
+                if (el.hasClass('popup-wrap')) {
+                    el.hide();
+                }
+            }
+        },
+
         onRender: function(){
             this.stage = new Konva.Stage({
                 container: this.$('#drawing').get(0)
@@ -21,18 +37,20 @@ var app = app || {};
             this.layer = new Konva.Layer();
             this.stage.add(this.layer);
 
-            this.heightInput = this.$('.heightInput').get(0);
-            this.widthInput = this.$('.widthInput').get(0);
+            this.heightInput = this.$('.heightInput');
+            this.widthInput = this.$('.widthInput')
 
-            this.widthInput.addEventListener('change', this.updateCanvas.bind(this));
-            this.widthInput.addEventListener('input', this.updateCanvas.bind(this));
+            this.widthInput.on('change input', function() {
+                this.active_drawing.set('width', parseInt(this.widthInput.val()))
+            }.bind(this));
 
-            this.heightInput.addEventListener('change', this.updateCanvas.bind(this));
-            this.heightInput.addEventListener('input', this.updateCanvas.bind(this));
+            this.heightInput.on('change input', function() {
+                this.active_drawing.set('height', parseInt(this.heightInput.val()))
+            }.bind(this));
 
             // set default value
-            this.widthInput.value = 1000;
-            this.heightInput.value = 2000;
+            this.widthInput.val(this.active_drawing.get('width'));
+            this.heightInput.val(this.active_drawing.get('height'));
 
 
             // do we have "afterRender" callback?!?
@@ -97,82 +115,86 @@ var app = app || {};
                 fill: 'lightblue'
             });
 
+
+            glass.on('click', this.showPopup.bind(this));
+
             group.add(glass, top, left, bottom, right);
 
             group.find('Line')
                 .closed(true)
                 .stroke('black')
-                .strokeWidth(1);
+                .strokeWidth(1)
+                .fill('white');
+
+            // draw mullions
+            var mullionWidth = 92;
+            var mullion;
+            if (this.active_drawing.get('verticalMullion')) {
+                var x = this.active_drawing.get('verticalMullionX');
+                mullion = new Konva.Rect({
+                    x: Math.round(x - mullionWidth / 2),
+                    y: padding,
+                    width: mullionWidth,
+                    height: frameHeight - padding * 2,
+                    stroke: 'black',
+                    fill: 'white',
+                    strokeWidth: 1
+                });
+                group.add(mullion);
+            } else if (this.active_drawing.get('horizontalMullion')) {
+                var y = this.active_drawing.get('horizontalMullionY');
+                console.log(y);
+                mullion = new Konva.Rect({
+                    x: padding,
+                    y: Math.round(y - mullionWidth / 2),
+                    width: frameWidth - padding * 2,
+                    height: mullionWidth,
+                    stroke: 'black',
+                    fill: 'white',
+                    strokeWidth: 1
+                });
+                group.add(mullion);
+            }
 
             return group;
         },
 
-        createInfo: function(frameWidth, frameHeight) {
-            var offset = 20;
-
-            var arrowOffset = offset / 2;
+        createVerticalMetric: function(width, height, modelMetric) {
+            var arrowOffset = width / 2;
             var arrowSize = 5;
-
             var group = new Konva.Group();
+
             var lines = new Konva.Shape({
                 sceneFunc: function(ctx) {
                     ctx.fillStyle = 'grey';
                     ctx.lineWidth = 0.5;
 
                     ctx.moveTo(0, 0);
-                    ctx.lineTo(-offset, 0);
+                    ctx.lineTo(width, 0);
 
-                    ctx.moveTo(0, frameHeight);
-                    ctx.lineTo(-offset, frameHeight);
-
-                    ctx.moveTo(0, frameHeight);
-                    ctx.lineTo(0, frameHeight + offset);
-
-                    ctx.moveTo(frameWidth, frameHeight);
-                    ctx.lineTo(frameWidth, frameHeight + offset);
+                    ctx.moveTo(0, height);
+                    ctx.lineTo(width, height);
 
                     ctx.stroke();
                 }
             });
 
-            var leftArrow = new Konva.Shape({
+            var arrow = new Konva.Shape({
                 sceneFunc: function(ctx) {
+                    ctx.translate(arrowOffset, 0);
                     // top pointer
-                    ctx.moveTo(-arrowOffset - arrowSize, arrowSize);
-                    ctx.lineTo(-arrowOffset, 0);
-                    ctx.lineTo(-arrowOffset + arrowSize, arrowSize);
-
-                    // line
-                    ctx.moveTo(-arrowOffset, 0);
-                    ctx.lineTo(-arrowOffset, frameHeight);
-
-                    // bottom pointer
-                    ctx.moveTo(-arrowOffset - arrowSize, frameHeight - arrowSize);
-                    ctx.lineTo(-arrowOffset, frameHeight);
-                    ctx.lineTo(-arrowOffset + arrowSize, frameHeight - arrowSize);
-
-                    ctx.strokeShape(this);
-                },
-                stroke: 'grey',
-                strokeWidth: 0.5
-            });
-
-            var bottomArrow = new Konva.Shape({
-                sceneFunc: function(ctx) {
-                    // top pointer
-                    ctx.translate(0, frameHeight + arrowOffset);
-                    ctx.moveTo(arrowSize, -arrowSize);
+                    ctx.moveTo(-arrowSize, arrowSize);
                     ctx.lineTo(0, 0);
                     ctx.lineTo(arrowSize, arrowSize);
 
                     // line
                     ctx.moveTo(0, 0);
-                    ctx.lineTo(frameWidth, 0);
+                    ctx.lineTo(0, height);
 
                     // bottom pointer
-                    ctx.moveTo(frameWidth - arrowSize, -arrowSize);
-                    ctx.lineTo(frameWidth, 0);
-                    ctx.lineTo(frameWidth - arrowSize, arrowSize);
+                    ctx.moveTo(-arrowSize, height - arrowSize);
+                    ctx.lineTo(0, height);
+                    ctx.lineTo(arrowSize, height - arrowSize);
 
                     ctx.strokeShape(this);
                 },
@@ -181,57 +203,157 @@ var app = app || {};
             });
 
             // left text
-            var leftLabel = new Konva.Label();
+            var label = new Konva.Label();
 
-            leftLabel.add(new Konva.Tag({
+            label.add(new Konva.Tag({
                 fill: 'white',
                 stroke: 'grey'
             }));
-            var leftText = new Konva.Text({
-                text: this.heightInput.value + 'mm',
+            var text = new Konva.Text({
+                text: this.active_drawing.get(modelMetric) + 'mm',
                 padding: 2,
                 fill: 'black'
             });
 
-            leftLabel.add(leftText);
-            leftLabel.position({
-                x: -arrowOffset - leftText.width(),
-                y: frameHeight / 2 - leftText.height() / 2
+            label.add(text);
+            label.position({
+                x: -text.width() / 2,
+                y: height / 2 - text.height() / 2
             });
 
 
-            leftLabel.on('click tap', function() {
-                this.createInput('height', leftLabel.getAbsolutePosition(), leftText.size());
+            label.on('click tap', function() {
+                this.createInput(modelMetric, label.getAbsolutePosition(), text.size());
             }.bind(this));
 
-            // bottom text
-            var bottomLabel = new Konva.Label();
+            group.add(lines, arrow, label);
+            return group;
+        },
 
-            bottomLabel.add(new Konva.Tag({
+        createHorizontalMetric: function(width, height, modelMetric) {
+            var arrowOffset = height / 2;
+            var arrowSize = 5;
+            var group = new Konva.Group();
+
+            var lines = new Konva.Shape({
+                sceneFunc: function(ctx) {
+                    ctx.fillStyle = 'grey';
+                    ctx.lineWidth = 0.5;
+
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(0, height);
+
+                    ctx.moveTo(width, 0);
+                    ctx.lineTo(width, height);
+
+                    ctx.stroke();
+                }
+            });
+
+            var arrow = new Konva.Shape({
+                sceneFunc: function(ctx) {
+                    // top pointer
+                    ctx.translate(0, arrowOffset);
+                    ctx.moveTo(arrowSize, -arrowSize);
+                    ctx.lineTo(0, 0);
+                    ctx.lineTo(arrowSize, arrowSize);
+
+                    // line
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(width, 0);
+
+                    // bottom pointer
+                    ctx.moveTo(width - arrowSize, -arrowSize);
+                    ctx.lineTo(width, 0);
+                    ctx.lineTo(width - arrowSize, arrowSize);
+
+                    ctx.strokeShape(this);
+                },
+                stroke: 'grey',
+                strokeWidth: 0.5
+            });
+
+            var label = new Konva.Label();
+
+            label.add(new Konva.Tag({
                 fill: 'white',
                 stroke: 'grey'
             }));
-            var bottomText = new Konva.Text({
-                text: this.widthInput.value + 'mm',
+            var text = new Konva.Text({
+                text: this.active_drawing.get(modelMetric) + 'mm',
                 padding: 2,
                 fill: 'black'
             });
 
-            bottomLabel.add(bottomText);
-            bottomLabel.position({
-                x: frameWidth / 2 - bottomText.width() / 2,
-                y: frameHeight + arrowOffset
+            label.add(text);
+            label.position({
+                x: width / 2 - text.width() / 2,
+                y: arrowOffset
             });
 
-            bottomLabel.on('click tap', function() {
-                this.createInput('width', bottomLabel.getAbsolutePosition(), bottomText.size());
+            label.on('click tap', function() {
+                this.createInput(modelMetric, label.getAbsolutePosition(), text.size());
             }.bind(this));
 
-            group.add(lines, leftArrow, bottomArrow, leftLabel, bottomLabel);
+            group.add(lines, arrow, label);
+            return group;
+        },
+
+        createInfo: function(frameWidth, frameHeight) {
+            var merticSize = 30;
+            var group = new Konva.Group();
+
+            var verticalWholeMertic = this.createVerticalMetric(merticSize, frameHeight, 'height');
+            verticalWholeMertic.position({
+                x: -merticSize,
+                y: 0
+            });
+
+            group.add(verticalWholeMertic);
+
+            var horizontalWholeMertic = this.createHorizontalMetric(frameWidth, merticSize, 'width');
+            horizontalWholeMertic.position({
+                x: 0,
+                y: frameHeight
+            });
+            group.add(horizontalWholeMertic);
+
+            if (this.active_drawing.get('horizontalMullion')) {
+                verticalWholeMertic.move({x: -merticSize});
+                var height = this.active_drawing.get('horizontalMullionY') * this.ratio;
+                var topMetric = this.createVerticalMetric(merticSize, height, 'horizontalMullionY');
+                topMetric.position({
+                    x: -merticSize,
+                    y: 0
+                });
+                // var bottomMetric = this.createVerticalMetric(merticSize, frameHeight / 2, 'verticalMullionSubX');
+                // bottomMetric.position({
+                //     x: -merticSize,
+                //     y: frameHeight / 2
+                // });
+                group.add(topMetric);
+
+            }
+
+            if (this.active_drawing.get('verticalMullion')) {
+                horizontalWholeMertic.move({y: merticSize});
+                var width = this.active_drawing.get('verticalMullionX') * this.ratio;
+                var leftMetric = this.createHorizontalMetric(width, merticSize, 'verticalMullionX');
+                leftMetric.position({
+                    x: 0,
+                    y: frameHeight
+                });
+                // var rightMetric = this.createHorizontalMetric(frameWidth / 2, merticSize);
+                // rightMetric.position({
+                //     x: frameWidth / 2,
+                //     y: frameHeight
+                // });
+                group.add(leftMetric);
+            }
 
             return group;
         },
-        createInput: function(metric, pos, size) {
+        createWrap: function() {
             var wrap = document.createElement('div');
             wrap.style.position = 'absolute';
             wrap.style.backgroundColor = 'rgba(0,0,0,0.1)';
@@ -242,12 +364,21 @@ var app = app || {};
             wrap.style.zIndex = 1000;
 
             document.body.appendChild(wrap);
+            wrap.addEventListener('click', function(e) {
+                if (e.target === wrap) {
+                    document.body.removeChild(wrap);
+                }
+            });
 
+            return wrap;
+        },
+        createInput: function(metric, pos, size) {
+            var wrap = this.createWrap();
             var input = document.createElement('input');
             input.type = 'number';
 
-            var similarInput = (metric === 'width' ? this.widthInput : this.heightInput);
-            input.value = similarInput.value;
+            // var similarInput = (metric === 'width' ? this.widthInput : this.heightInput);
+            input.value = this.active_drawing.get(metric);
 
             input.style.position = 'absolute';
             input.style.top = pos.y + this.el.offsetTop + 3 + 'px';
@@ -259,20 +390,13 @@ var app = app || {};
             wrap.appendChild(input);
 
             input.addEventListener('change', function() {
-                similarInput.value = input.value;
-                this.updateCanvas();
+                this.active_drawing.set(metric, input.value);
             }.bind(this));
 
             input.addEventListener('input', function() {
-                similarInput.value = input.value;
-                this.updateCanvas();
+                this.active_drawing.set(metric, input.value);
             }.bind(this));
 
-            wrap.addEventListener('click', function(e) {
-                if (e.target === wrap) {
-                    document.body.removeChild(wrap);
-                }
-            });
 
 
             input.addEventListener('keyup', function(e) {
@@ -281,12 +405,25 @@ var app = app || {};
                 }
             });
         },
+        showPopup: function(e) {
+            var pos = this.stage.getPointerPosition();
+            var x = pos.x - 5;
+            var y = pos.y - 5;
 
+            var $popupWrap = this.$('.popup-wrap');
+            $popupWrap
+            .show()
+            .find('.popup')
+            .css({
+                top: y,
+                left: x
+            });
+        },
         updateCanvas: function() {
             this.layer.children.destroy();
 
-            var frameWidth = parseInt(this.widthInput.value, 10);
-            var frameHeight = parseInt(this.heightInput.value, 10);
+            var frameWidth = this.active_drawing.get('width');
+            var frameHeight = this.active_drawing.get('height');
 
 
 
@@ -297,6 +434,7 @@ var app = app || {};
 
             var frameOnScreenWidth = frameWidth * ratio;
             var frameOnScreenHeight = frameHeight * ratio;
+            this.ratio = ratio;
 
             var group = new Konva.Group({
             });
@@ -314,6 +452,24 @@ var app = app || {};
             group.add(infoGroup);
 
             this.layer.draw();
+        },
+
+        addVerticalMullion: function() {
+            console.log(this.active_drawing.get('width') / 2, '!!');
+            this.active_drawing.set({
+                verticalMullion: true,
+                horizontalMullion: false,
+                verticalMullionX: this.active_drawing.get('width') / 2
+            });
+            this.$('.mullion-wrap').hide();
+        },
+        addHorizontalMullion: function() {
+            this.active_drawing.set({
+                verticalMullion: false,
+                horizontalMullion: true,
+                horizontalMullionY: this.active_drawing.get('height') / 2
+            });
+            this.$('.mullion-wrap').hide();
         }
     });
 })();
