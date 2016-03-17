348e66d4c6a746279fa8f26fa68160a855abc16c
Anton Vasyunin <thevasya@gmail.com>
2016-03-17 12:33:34 +0600

    Add visual test viewer

---
+++ test/visual-test-runner/test-visual.js
@@ -0,0 +1,12 @@
+/* global app */
+/* eslint-env qunit */
+/* eslint strict:0 */
+/* eslint max-statements:0 */
+/* jscs:disable */
+
+
+//  Test that QUnit is working
+test('basic test', function () {
+    ok(true, 'Passed.');
+});
+
