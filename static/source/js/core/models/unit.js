0bded7b26940ec3735e2ad38577f8a75910967ad
Denis Shmakov <uoowuo@zoho.com>
2017-03-23 21:44:42 +0300

    Overlay metrics for mulled operable sash are incorrect #518 - removed partial solution to (1)

--- static/source/js/core/models/unit.js
+++ static/source/js/core/models/unit.js
@@ -1261,52 +1261,52 @@ var app = app || {};
                 rootSection.mullionParams = mullionAttrs;
             }
 
-            // Vars for use in the following map callback
-            var parentHasFrame = hasFrame;
-
             rootSection.sections = _.map(rootSection.sections, function (sectionData, i) {
-
-                var sectionParams = { x: null, y: null, width: null, height: null };
-                var isVertical = rootSection.divider === 'vertical' || rootSection.divider === 'vertical_invisible';
-                var isHorizontal = rootSection.divider === 'horizontal' || rootSection.divider === 'horizontal_invisible';
-                var isFirst = i === 0;
-                var isLeft = isVertical && !isFirst;  // Is this left section when looking from outside?
-                var isRight = isVertical && isFirst;
-                var isTop = isHorizontal && isFirst;
-                var isBottom = isHorizontal && !isFirst;
-                var isOperable = _.contains(OPERABLE_SASH_TYPES, sectionData.sashType);
-                var sashFrameWidth = this.profile.get('sash_frame_width');
-                var sashFrameOverlap = this.profile.get('sash_frame_overlap');
-                var sashMullionOverlap = this.profile.get('sash_mullion_overlap');
-                var sashFrameGlassOverlap = sashFrameWidth - sashFrameOverlap;
-                var trim = function (amount, sides) {
-                    if (sides === 'all') { sides = ['top', 'right', 'bottom', 'left']; }
-                    if (_.isString(sides)) { sides = [sides]; }
-
-                    sides.forEach(function (side) {
-                        if (side === 'top') {
-                            sectionParams.y += amount;
-                            sectionParams.height -= amount;
-                        } else if (side === 'right') {
-                            sectionParams.x += amount;
-                            sectionParams.width -= amount;
-                        } else if (side === 'bottom') {
-                            sectionParams.height -= amount;
-                        } else if (side === 'left') {
-                            sectionParams.width -= amount;
-                        }
-                    });
+                var sectionParams = {
+                    x: null, y: null, width: null, height: null
                 };
 
-                // Set section data
                 sectionData.mullionEdges = _.clone(rootSection.mullionEdges);
                 sectionData.thresholdEdge = rootSection.thresholdEdge;
                 sectionData.parentId = rootSection.id;
-                if (isVertical) {
+
+                // Correction params. Needed for sections in operable sash
+                var corr = -1 * (this.profile.get('sash_frame_width') - this.profile.get('sash_frame_overlap'));
+                var correction = {
+                    x: 0,
+                    y: 0,
+                    width: 0,
+                    height: 0
+                };
+
+                // Calculate correction params
+                if (rootSection.sashType !== 'fixed_in_frame') {
+                    if (rootSection.divider === 'vertical' || rootSection.divider === 'vertical_invisible') {
+                        // correction for vertical sections
+                        if (i === 0) {
+                            correction.x = -1 * corr;
+                        }
+
+                        correction.y = -1 * corr;
+                        correction.width = corr;
+                        correction.height = corr * 2;
+                    } else {
+                        // correction for horizontal sections
+                        if (i === 0) {
+                            correction.y = -1 * corr;
+                        }
+
+                        correction.x = -1 * corr;
+                        correction.width = corr * 2;
+                        correction.height = corr;
+                    }
+                }
+
+                if (rootSection.divider === 'vertical' || rootSection.divider === 'vertical_invisible') {
                     sectionParams.x = openingParams.x;
                     sectionParams.y = openingParams.y;
 
-                    if (isFirst) {
+                    if (i === 0) {
                         sectionParams.width = position - rootSection.openingParams.x -
                             this.profile.get('mullion_width') / 2;
                         sectionData.mullionEdges.right = rootSection.divider;
@@ -1323,7 +1323,7 @@ var app = app || {};
                     sectionParams.y = openingParams.y;
                     sectionParams.width = openingParams.width;
 
-                    if (isFirst) {
+                    if (i === 0) {
                         sectionData.mullionEdges.bottom = rootSection.divider;
                         sectionParams.height = position - rootSection.openingParams.y -
                             this.profile.get('mullion_width') / 2;
@@ -1336,18 +1336,11 @@ var app = app || {};
                     }
                 }
 
-                // Trim glasses inside subdivided framed sashes
-                if (parentHasFrame && isLeft) {
-                    trim(sashFrameGlassOverlap, ['bottom', 'left', 'top']);
-                    trim(sashMullionOverlap, 'bottom');
-                } else if (parentHasFrame && isRight) {
-                    trim(sashFrameGlassOverlap, ['top', 'right', 'bottom']);
-                    trim(sashMullionOverlap, 'bottom');
-                } else if (parentHasFrame && isTop) {
-                    trim(sashFrameGlassOverlap, ['left', 'top', 'right']);
-                } else if (parentHasFrame && isBottom) {
-                    trim(sashFrameGlassOverlap, ['right', 'bottom', 'left']);
-                }
+                // Apply corrections
+                sectionParams.x += correction.x;
+                sectionParams.y += correction.y;
+                sectionParams.width += correction.width;
+                sectionParams.height += correction.height;
 
                 return this.generateFullRoot(sectionData, sectionParams);
             }.bind(this));
